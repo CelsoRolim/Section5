dbc44dff62ce1781d6963fba84c8b527262c29ca
Celso <Celso>
2016-06-19 16:58:42 -0400

    Title: Added flow between scenes. Start to Level_01 and Win to Start Developer: Celso

---
+++ Assets/Scripts/BlockSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BlockSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Method to go to a specific scene.
+    public void goToScene(string sceneName) {
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
