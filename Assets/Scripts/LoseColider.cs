b7dab6e5990de837517fcf44dc2c8f42be99d591
Celso <Celso>
2016-06-21 22:29:05 -0400

    Title: Added event for collision(Collision and Trigger) Developer: Celso

---
+++ Assets/Scripts/LoseColider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseColider : MonoBehaviour
+{
+
+    // Trigger collider ignores the physics (the ball pass throught the LoseObject)
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        Debug.Log("OnTriggerEnter2D");
+    }
+
+    // Collision collider consider the physics of the object(the ball stops on the LoseObject)
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D");
+    }
+}
