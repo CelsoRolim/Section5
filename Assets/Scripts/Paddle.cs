8a8e588520c27b605004a3280920ae2ec8b83a85
Celso <Celso>
2016-06-22 22:41:21 -0400

    Title: Added interaction between ball and paddle. Paddle is moved by the mouse. Developer: Celso

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mousePosInBox = ((Input.mousePosition.x / Screen.width) * 16);
+        mousePosInBox = Mathf.Clamp(mousePosInBox, 0.5f, 15.5f);
+
+        Vector3 position = new Vector3(mousePosInBox, this.transform.position.y, 0f);
+
+        this.transform.position = position;
+    }
+}
